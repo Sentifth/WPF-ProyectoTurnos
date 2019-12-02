c2c7b6c6236e4266bd6df174d1ecca85b86493ce
Jayro Paúl Guzmán Murguía <jguzman14@ucol.mx>
2019-12-02 02:02:26 -0600

    FFF

--- TurnosWPF/TurnosWPF/frm_CrearTickets.xaml.cs
+++ TurnosWPF/TurnosWPF/frm_CrearTickets.xaml.cs
@@ -48,49 +48,12 @@ namespace TurnosWPF
 
 
         }
-        
+
 
 
         private void Btn_CrearTicket(object sender, RoutedEventArgs e)
         {
-            try
-            {
-                this.btn_CrearTicket.Enabled = false;
-                    if (this.dataGridView1.Rows.Count != 0)
-                    {
-                        int index = this.dataGridView1.CurrentRow.Index;
-                        this.Tick.codigo = this.dataGridView1.Rows[index].Cells[1].Value.ToString();
-                        DataTable dataTable = this.Tick.sp_registrar_ticket();
-                        long num = Convert.ToInt64(dataTable.Rows[0][1].ToString());
-                        dataTable = this.Tick.sp_listar_tickets_xcodigo();
-                        for (int i = 0; i < dataTable.Rows.Count; i++)
-                        {
-                            if (num == Convert.ToInt64(dataTable.Rows[i][0].ToString()))
-                            {
-                                this.Tick.id = Convert.ToInt64(dataTable.Rows[i][0].ToString());
-                                this.Tick.codigo = dataTable.Rows[i][1].ToString();
-                                this.Tick.codigo_numeracion = this.Tick.codigo + "-" + (i + 1);
-                                this.Tick.modulo = (int)Convert.ToInt16(dataTable.Rows[i][3].ToString());
-                                this.Tick.estado = (int)Convert.ToInt16(dataTable.Rows[i][4].ToString());
-                                this.Tick.sp_actualizar_ticket();
-                                new frm_Imprimir_Ticket
-                                {
-                                    Id = this.Tick.id,
-                                    Clasificacion = this.dataGridView1.Rows[index].Cells[2].Value.ToString(),
-                                    NumeroTicket = this.Tick.codigo_numeracion
-                                }.ShowDialog();
-                            }
-                        }
-                    }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Error: btn_CrearTicket_Click \n\n" + ex.Message, "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Hand);
-            }
-            finally
-            {
-                this.btn_CrearTicket.Enabled = true;
-            }
+          
         }
 
     }
